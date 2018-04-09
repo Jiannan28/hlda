257e3c741c03409e44322250e1b78eaf67bac0d8
Jiannan Liu <Jiannan Liu@fp.manulife.hk>
2018-04-09 22:58:26 +0800

    patch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name = 'py3hlda',
 	packages = ['py3hlda'],
-	version = '0.1',
+	version = '0.1.1',
 	description = 'Gibbs sampler for the Hierarchical Latent Dirichlet Allocation topic model. This is based on the hLDA implementation from Mallet, having a fixed depth on the nCRP tree.',
 	author = 'co',
 	url = 'https://github.com/Jiannan28/hlda', 
